5312ccc986e02d6009389a0dc5d7b5fcfa12b899
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2024-07-06 23:47:54 -0400

    Updated python script

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 
+file_number = input("Enter the Page Number that You scraped")
 def removal(path):   #to remove any ' from the string so that it doesnot stop
     with open(path, 'r') as file:
         content = file.read()
@@ -123,4 +124,5 @@ df = pd.DataFrame({
 })
 
 print(df)
-df.to_csv("AI-Cincinati-Page().csv")
+output_text = "AI-Cincinati-Page(" + file_number +").csv"
+df.to_csv(output_text)
